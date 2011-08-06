1043974a079e060ec1090d7ad2d4e67babef349c
James May <monooptical@gmail.com>
2011-08-06 01:28:20 -0400

    New Placeholder Page
    1st try

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
-coming soon...
+<!DOCTYPE html>
+<html>
+  <head>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js" type="text/javascript" charset="utf-8"></script>   
+	<link rel="stylesheet" href="_assets/css/style.css" type="text/css" media="screen" title="main css" charset="utf-8">
+	
+  </head>
+  <body>
+    <div id="wrapper">
+		<div id="logo"><img src="_assets/img/logo.png" width="291" height="63" alt="Monooptical"></div>
+		<div id="me"><img src="_assets/img/me.png" width="296" height="384" alt="James May"></div>
+		<div id="bubble">
+			<h2>Thanks for stopping by!</h2>
+			<p>Unfortunately, the thing about being busy is my site tends to go by the wayside. Stay tuned, I will get something up soon... Hopefully :)
+			</p>
+		</div>
+		<div id="bar">
+			<p>I do find <span class="emphasis">some</span> time for these:</p>
+			<ul>
+				<a href="https://plus.google.com/102679125590006441548"><li id= "plus">Google Plus</li></a>
+				<a href="http://twitter.com/#!/monooptical"><li id="twitter">Twitter</li></a>
+				<a href="http://dribbble.com/monooptical"><li id="dribbble">Dribbble</li></a>
+			</ul>
+		</div>
+	</div>
+  </body>
+</html>
